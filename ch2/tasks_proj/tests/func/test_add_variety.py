6dd3bf6b716fcc0cbc97d59f019225681c61cde5
tinghsiangyang <tinghsiangyang@qq.com>
2021-05-24 22:08:53 +0800

    53页

--- ch2/tasks_proj/tests/func/test_add_variety.py
+++ ch2/tasks_proj/tests/func/test_add_variety.py
@@ -31,3 +31,29 @@ def test_add_2(task):
     task_id = tasks.add(task)
     t_from_db = tasks.get(task_id)
     assert equivalent(t_from_db, task)
+
+
+@pytest.mark.parametrize('summary, owner, done',
+                        [('sleep', None, False),
+                        ('wake', 'brian', False),
+                        ('breathe', 'NRIAN', True),
+                        ('eat eggs', 'BrIaN', False)])
+def test_add_3(summary, owner, done):
+    task = Task('summary', owner, done)
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(task, t_from_db)
+
+
+tasks_to_try = [Task('sleep', done=True),
+                Task('wake', 'brian'),
+                Task('wake', 'brian'),
+                Task('breathe', 'BRIAN', True),
+                Task('exercise', 'BrIaM', False)]
+
+
+@pytest.mark.parametrize('task', tasks_to_try)
+def test_add_4(task):
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
